3adcd524232c4f3a2d5e9c9547819003e343f7c8
AzucenaPA <m4o1800o2423c@mooc-cuaed.unam.mx>
2024-04-30 23:32:52 -0600

    uso de prompt para un array

--- ejercicios-logica-UserProfile.js
+++ ejercicios-logica-UserProfile.js
@@ -1,7 +1,9 @@
 //USER PROFILE
 let userName = prompt("Escribe el nombre de usuario:");
 let age = prompt("Escribe cuántos años tienes:");
-let favoriteMovies = ["Cuestión de tiempo", "TOC TOC", "Juego de gemelas"];
+let favoriteMovies = prompt("Escribe las tus películas favoritas separadas por comas y sin espacio después de la coma:");
+favoriteMovies = favoriteMovies.split(',');
+console.log(favoriteMovies);
 
 
 console.log(`Hola! Mi nombre de usuario es ${userName} y tengo ${age} años.`);
